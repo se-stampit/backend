5a80dfae59da6306cc4115e78c1d418f25ff69f4
Jakob Mayer <jakob.mayer@slidelizard.at>
2016-04-19 15:10:59 +0200

    Initial project structure

---
+++ Stampit/Stampit.Webapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stampit.Webapp.MvcApplication" Language="C#" %>
---
+++ Stampit/Stampit.Webapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stampit.Webapp.Startup))]
+namespace Stampit.Webapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
