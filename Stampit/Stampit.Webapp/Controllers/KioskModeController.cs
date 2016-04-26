3b1c019b9a4d290c8be4686fdee70a13eb659042
Jakob Mayer <jakob.mayer@slidelizard.at>
2016-04-26 02:54:03 +0200

    imported first kiosk mode view and basic, not productive, controller logic

--- Stampit/Stampit.Webapp/Controllers/KioskModeController.cs
+++ Stampit/Stampit.Webapp/Controllers/KioskModeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Stampit.Webapp.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,53 @@ namespace Stampit.Webapp.Controllers
 {
     public class KioskModeController : Controller
     {
-        // GET: KioskMode
+        private const string SESSION_STATE = "SessionState";
+
         public ActionResult Index()
         {
-            return View();
+            var sessionState = Session[SESSION_STATE] as SessionState;
+            if (sessionState == null)
+            {
+                sessionState = new SessionState
+                {
+                    Model = new List<ProductViewModel>
+                    {
+                        new ProductViewModel { Name = "Pizza", Count = 0 },
+                        new ProductViewModel { Name = "Kebab", Count = 0 }
+                    },
+                    SelectedViewModel = null
+                };
+                Session[SESSION_STATE] = sessionState;
+            }
+
+            return View(sessionState.Model);
         }
+
+        public ActionResult SetCount(int count)
+        {
+            var sessionState = Session[SESSION_STATE] as SessionState;
+            if (sessionState == null || sessionState.SelectedViewModel == null) return RedirectToAction("Index");
+
+            sessionState.SelectedViewModel.Count = count;
+            Session[SESSION_STATE] = sessionState;
+
+            return View("Index", sessionState.Model);
+        }
+
+        public ActionResult SelectProductViewModel(string selectedProduct)
+        {
+            var sessionState = Session[SESSION_STATE] as SessionState;
+            if (sessionState == null || sessionState.Model == null) return RedirectToAction("Index");
+
+            sessionState.SelectedViewModel = sessionState.Model.Where(vm => vm.Name == selectedProduct).FirstOrDefault();
+            Session[SESSION_STATE] = sessionState;
+            return View("Index", sessionState.Model);
+        }
+    }
+
+    public class SessionState
+    {
+        public IEnumerable<ProductViewModel> Model { get; set; }
+        public ProductViewModel SelectedViewModel { get; set; }
     }
 }
