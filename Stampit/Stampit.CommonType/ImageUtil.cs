f88c6cbd2fdc392c8aabdfe2517feedab282d667
Jakob Mayer <jakob.mayer@slidelizard.at>
2016-04-27 18:41:38 +0200

    basic kioskmode flow implemented with fake data -> generate stamps -> generate qrcode

---
+++ Stampit/Stampit.CommonType/ImageUtil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stampit.CommonType
+{
+    public static class ImageUtil
+    {
+        public static async Task<byte[]> GetImageFromUrl(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                throw new ArgumentNullException(nameof(url));
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                url = Uri.EscapeUriString(url);
+                if(!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                    throw new ArgumentException($"The given url ({url}) is not well formed", nameof(url));
+            }
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+                httpWebRequest.Proxy = null;
+                using (var httpWebReponse = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+                    using (var imgStream = httpWebReponse.GetResponseStream())
+                    {
+                        using (var ms = new MemoryStream())
+                        {
+                            await imgStream.CopyToAsync(ms);
+                            return ms.ToArray();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Stampit/Stampit.Logic.Interface/IQrCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stampit.Logic.Interface
+{
+    /// <summary>
+    /// Defines an interface to generate QR codes based on a given content
+    /// </summary>
+    public interface IQrCodeGenerator
+    {
+        /// <summary>
+        /// Returns a valid url directing to an image containing the specified qrcode with the given content
+        /// </summary>
+        /// <param name="content">A string representation of the content, the qr code should contain</param>
+        /// <param name="width">The width of the requested qrcode, should be the same as the param height to be square formed</param>
+        /// <param name="height">The height of the requested qrcode, should be the same as the param width to be square formed</param>
+        /// <returns>The url to request the byte array containing the image</returns>
+        string GetQrCodeUrl(string content, int width = 300, int height = 300);
+    }
+}
