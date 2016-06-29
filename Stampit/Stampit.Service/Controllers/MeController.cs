65b50f0bbbd744976cbc6fe0b6922c0aca777056
Jakob Mayer <jakob.mayer@slidelizard.at>
2016-06-29 12:58:39 +0200

    delete stampcard implemented

--- Stampit/Stampit.Service/Controllers/MeController.cs
+++ Stampit/Stampit.Service/Controllers/MeController.cs
@@ -105,6 +105,19 @@ namespace Stampit.Service.Controllers
             return Content(HttpStatusCode.OK, new { count = stampcards.Count() });
         }
 
+        [HttpDelete]
+        [Route("api/me/stampcard/{stampcardid}")]
+        public async Task<IHttpActionResult> DeleteStampcard(string stampcardid)
+        {
+            var usermail = Request.GetOwinContext().Environment[Setting.AUTH_ENVIRONMENT_ID]?.ToString();
+            var enduser = await EnduserRepository.FindByMailAddress(usermail);
+            var stampcard = await StampcardRepository.FindByIdAsync(stampcardid);
+            if (stampcard == null || stampcard.EnduserId != enduser.Id)
+                return Content(HttpStatusCode.BadRequest, new { errorMessage = "The given stampcardid is not valid! This stampcard does not exist" });
+            await StampcardRepository.Delete(stampcard);
+            return Content(HttpStatusCode.NoContent, new { });
+        }
+
         public class StampcodeDTO
         {
             [JsonProperty("stampcode")]
